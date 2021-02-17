fe62d759f794560ecb3a12fb8b8c9ccbcb997220
Tash Alderson <aldersonnatasha@gmail.com>
2021-02-17 21:16:03 +0000

    city-api

---
+++ db/data/cityData.js
@@ -0,0 +1,124 @@
+export default function getCityData() {
+  return [
+    {
+      city: 'Amsterdam',
+      about: '',
+      country: 'The Netherlands',
+      currency: 'Euro',
+      language: 'Dutch',
+      image: ''
+    },
+    {
+      city: 'Berlin',
+      about: '',
+      country: 'Germany',
+      currency: 'Euro',
+      language: 'German',
+      image: ''
+    },
+    {
+      city: 'Copenhagen',
+      about: '',
+      country: 'Denmark',
+      currency: 'Danish Krone',
+      language: 'Danish',
+      image: ''
+    },
+    {
+      city: 'London',
+      about: '',
+      country: 'England',
+      currency: 'Pound Sterling',
+      language: 'English',
+      image: ''
+    },
+    {
+      city: 'Barcelona',
+      about: '',
+      country: 'Spain',
+      currency: 'Euro',
+      language: 'Spanish',
+      image: ''
+    },
+    {
+      city: 'Paris',
+      about: '',
+      country: 'France',
+      currency: 'Euro',
+      language: 'French',
+      image: ''
+    },
+    {
+      city: 'Prague',
+      about: '',
+      country: 'Czech Republic',
+      currency: 'Czech',
+      language: 'Korunas',
+      image: ''
+    },
+    {
+      city: 'Lisbon',
+      about: '',
+      country: 'Portugal',
+      currency: 'Euro',
+      language: 'Portuguese',
+      image: ''
+    },
+    {
+      city: 'Oslo',
+      about: '',
+      country: 'Norway',
+      currency: 'Kroner',
+      language: 'Norwegian',
+      image: ''
+    },
+    {
+      city: 'Madrid',
+      about: '',
+      country: 'Spain',
+      currency: 'Euro',
+      language: 'Spanish',
+      image: ''
+    },
+    {
+      city: 'Rome',
+      about: '',
+      country: 'Italy',
+      currency: 'Euro',
+      language: 'Italian',
+      image: ''
+    },
+    {
+      city: 'Athens',
+      about: '',
+      country: 'Greece',
+      currency: 'Euro',
+      language: 'Greek',
+      image: ''
+    },
+    {
+      city: 'Stockholm',
+      about: '',
+      country: 'Sweden',
+      currency: 'Swedish Krona',
+      language: 'Swedish',
+      image: ''
+    },
+    {
+      city: 'Istanbul',
+      about: '',
+      country: 'Turkey',
+      currency: 'Turkish lira',
+      language: 'Turkish',
+      image: ''
+    },
+    {
+      city: 'Budapest',
+      about: '',
+      country: 'Hungary',
+      currency: 'Hungarian Forints',
+      language: 'Hungarian',
+      image: ''
+    }
+  ]
+}
