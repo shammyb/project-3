0e6d985546493e5fd569224588a14b7251997ed4
aosman <pianogreen0@gmail.com>
2021-02-18 14:05:10 +0000

    routes

--- views/router.js
+++ views/router.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import city from '../controllers/city.js'
+import user from '../controllers/user.js'
+import comment from '../controllers/comment.js'
 
 
 
@@ -9,5 +12,31 @@ import express from 'express'
 
 const router = express.Router()
 
+
+router.route('/cityscapes')
+  .get(city.getCity)
+  .post(city.makeCity)
+
+
+
+router.route('/cityscapes/:id')
+  .get(city.getSingleCity)
+  .put(city.updateCity)
+  .delete(city.removeCity)
+
+
+router.route('/register')
+  .post(user.register)
+
+router.route('/login')
+  .post(user.login)
+
+
+router.route('cityscapes/:cityid/comment')
+  .post(comment.makeComment)
+
+router.route('cityscapes/:cityid/comment/:commentId')
+  .put(comment.updateComment)
+  .delete(comment.removeComment)
 
 export default router
