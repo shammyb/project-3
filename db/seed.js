15b286a6800979ad6635dff4b064ebdb3852fdc0
Benjamin Shamash <benjaminshamash@gmail.com>
2021-02-18 14:12:42 +0000

    my great new feature

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,10 @@
-// ! Mongoose and ability to connect to mongodb
+
 import mongoose from 'mongoose'
 import connectToDb from '../lib/connectToDb.js'
-// ! Models to use to post to mongo db
+
 import City from '../models/city.js'
 import User from '../models/user.js'
-// ! Data to seed
+
 import getCityData from './data/cityData.js'
 import getUserData from './data/userData.js'
 
@@ -14,7 +14,7 @@ async function seedDatabase() {
 
     console.log('🤖 Database connected!')
 
-    // ! Delete all the data in our database -- we dont need to do this.
+  
     await mongoose.connection.db.dropDatabase()
 
     console.log('🤖 Database was dropped!')
@@ -29,8 +29,7 @@ async function seedDatabase() {
 
     console.log(`🤖 ${city.length} city created!`)
 
-    // ! This line to close the connection to mongodb, good practice,
-    // ! prevents mongodb from having a bunch of useless open connections.
+
     await mongoose.connection.close()
     console.log('🤖 Goodbye!')
 
