f0f49fa3f9ac8f6747c1c3646966713aad23cb60
aosman <pianogreen0@gmail.com>
2021-02-18 14:34:30 +0000

    added

--- db/data/cityData.js
+++ db/data/cityData.js
@@ -14,7 +14,7 @@ export default function getCityData() {
     },
     {
       city: 'Berlin',
-      about: 'Berlin, Germany’s capital, dates to the 13th century. Reminders of the city's turbulent 20th-century history include its Holocaust memorial and the Berlin Wall's graffitied remains. Divided during the Cold War, its 18th-century Brandenburg Gate has become a symbol of reunification. The citys also known for its art scene and modern landmarks like the gold-colored, swoop-roofed Berliner Philharmonie, built in 1963.',
+      about: 'Berlin, Germany’s capital, dates to the 13th century. Reminders of the city\'s turbulent 20th-century history include its Holocaust memorial and the Berlin Wall\'s graffitied remains. Divided during the Cold War, its 18th-century Brandenburg Gate has become a symbol of reunification. The citys also known for its art scene and modern landmarks like the gold-colored, swoop-roofed Berliner Philharmonie, built in 1963.',
       country: 'Germany',
       currency: 'Euro',
       continent: 'Europe',
