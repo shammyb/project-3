57f0681c009b1b3e06badeb354de45ec1533cb12
Benjamin Shamash <benjaminshamash@gmail.com>
2021-02-19 12:18:58 +0000

    my great new feature

--- client/src/components/PostCity.js
+++ client/src/components/PostCity.js
@@ -1,10 +1,48 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+import CityForm from './CityForm'
+
 
 export default function PostCity() {
-  return <section>
+  const [formData, updateFormData] = useState({
+    city: '',
+    about: '',
+    country: '',
+    currency: '',
+    continent: '',
+    language: '',
+    image: '',
+    long: '',
+    lat: ''
+  })
 
+  function handleChange(event) {
+    updateFormData({ ...formData, [event.target.name]: event.target.value })
+  }
 
-  </section>
+  async function handleSubmit(event) {
+    event.preventDefault()
+    const token = localStorage.getItem('token')
+
+    const newFormData = {
+      ...formData
+    }
+
+    try {
+      const { data } = await axios.post('/api/cityscapes', newFormData, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      console.log(data._id)
+      history.push(`/cityscapes/${data._id}`)
+    } catch (err) {
+      console.log(err.response.data)
+    }
+  }
+
+  // ! Our form is now a shared component!
+  return <CityForm
+    handleChange={handleChange}
+    handleSubmit={handleSubmit}
+    formData={formData}
+  />
 }
