94766537f64f1c76b84efda1ea23908524dd7783
Tash Alderson <aldersonnatasha@gmail.com>
2021-02-19 13:57:15 +0000

    individual-city

--- client/src/components/City.js
+++ client/src/components/City.js
@@ -1,11 +1,79 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+import { useHistory } from "react-router-dom"
 
-export default function City() {
-  return <section>
+export default function City({ match }) {
 
-    
+  const cityName = match.params.cityName
+  const [city, updateCity] = useState({})
+
+  useEffect(() => {
+    async function fetchCityData() {
+      try {
+        const { data } = await axios.get(`/api/cityscapes/discover/${cityName}`)
+        updateCity(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchCityData()
+  }, [])
+
+  const history = useHistory()
+
+  const routeChangeTTD = () => {
+    const pathTTD = '/cityscapes/discover/:cityname/thingstodo'
+    history.push(pathTTD)
+  }
+  const routeChangeFlights = () => {
+    const pathFlights = '/cityscapes/flights'
+    history.push(pathFlights)
+  }
+  const routeChangeRestaurants = () => {
+    const pathRestaurants = '/cityscapes/discover/:cityname/restaurants'
+    history.push(pathRestaurants)
+  }
+  const routeChangeExperiences = () => {
+    const pathExperiences = '/cityscapes/discover/:cityname/experiences'
+    history.push(pathExperiences)
+  }
+
+  return <section >
+    <div className="name-image-about">
+      <h1>{city.city}</h1>
+      <img src={city.image} alt={city.name} />
+      <p>{city.about}</p>
+    </div>
+    <div className="search-buttons">
+      <div className="things-to-do">
+        <h2>Search Things to Do</h2>
+        <img />
+        <button onClick={routeChangeTTD}>
+          Search
+        </button>
+      </div>
+      <div className="flights">
+        <h2>Search Flights</h2>
+        <img />
+        <button onClick={routeChangeFlights}>
+          Search
+        </button>
+      </div>
+      <div className="restaurants">
+        <h2>Search Restaurants</h2>
+        <img />
+        <button onClick={routeChangeRestaurants}>
+          Search
+        </button>
+      </div>
+      <div className="experiences">
+        <h2>Search Experiences</h2>
+        <img />
+        <button onClick={routeChangeExperiences}>
+          Search
+        </button>
+      </div>
+    </div>
+
   </section>
-
 }
