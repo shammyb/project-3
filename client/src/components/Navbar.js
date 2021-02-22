7a8e3311bd0775d1706ca6f638d619ab61779a7f
Tash Alderson <aldersonnatasha@gmail.com>
2021-02-22 14:15:35 +0000

    navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,8 +6,29 @@ import axios from 'axios'
 
 
 export default function Navbar() {
-  return <section>
-
-
-  </section>
+return <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div className="navbar-menu is-active">
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="buttons">
+            <Link to="/cityscapes" className="button is-dark">
+              Home
+            </Link>
+            <Link to='/cityscapes/discover' className="button is-primary">
+              Discover
+            </Link>
+            <Link to='/cityscapes/flights' className="button is-primary">
+              Flights
+            </Link>
+            <Link to="/register" className="button is-light">
+              Register
+            </Link>
+            <Link to="/login" className="button is-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 }
